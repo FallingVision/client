983ef9ad6a5f7f727dae75e95c5fc046a1424881
BeomChang <fnzlvkdvkd@naver.com>
2021-07-15 16:58:50 +0900

    pod install

---
+++ src/components/screens/camera/CameraScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const CameraScreen = (): JSX.Element => {
+  return (
+    <View>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+    </View>
+  );
+};
+
+export default CameraScreen;
