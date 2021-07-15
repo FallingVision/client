003eea5c6dfe50ca5c8aca7c1c8577d13dcf67c0
BeomChang <fnzlvkdvkd@naver.com>
2021-07-15 17:05:53 +0900

    eslint JSX 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  globals: {
+    React: true,
+    google: true,
+    mount: true,
+    mountWithRouter: true,
+    shallow: true,
+    shallowWithRouter: true,
+    context: true,
+    expect: true,
+    jsdom: true,
+    JSX: true,
+  },
 };
--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import CameraScreen from './src/components/screens/camera/CameraScreen';
 
 const App = () => {
   return (
     <SafeAreaView>
       <Text>Falling Vision</Text>
+      <CameraScreen />
     </SafeAreaView>
   );
 };
